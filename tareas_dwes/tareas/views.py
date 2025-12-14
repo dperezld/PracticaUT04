f2f298229ee2e38557c7d53e0c457cdabeb7b395
dperezld <dperezld@fpvirtualaragon.es>
2025-12-14 21:24:25 +0100

    visa funciona

--- tareas_dwes/tareas/views.py
+++ tareas_dwes/tareas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import DetailView
+from .models import Tarea
+
+class TareaDetailView(DetailView):
+    model = Tarea
+    template_name = 'tareas/detalle_tarea.html' # Ruta donde buscaremos el HTML
+    context_object_name = 'tarea' # Nombre de la variable a usar en el HTML
