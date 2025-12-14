17c90f1e4e232f9a0fd9fa017986766727a1183f
dperezld <dperezld@fpvirtualaragon.es>
2025-12-14 21:17:36 +0100

    modelo funcionando

--- tareas_dwes/tareas/models.py
+++ tareas_dwes/tareas/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Tarea(models.Model):
+    # UUIDField es más seguro y único que un ID numérico simple
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    titulo = models.CharField(max_length=200, verbose_name="Título")
+    descripcion = models.TextField(verbose_name="Descripción")
+    
+    # Por defecto la tarea no está completada
+    completada = models.BooleanField(default=False)
+    
+    # auto_now_add guarda la fecha exacta en que se crea el objeto
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    # Fecha recordatorio manual. null=True permite que esté vacío en la BD si no se define.
+    fecha_recordatorio = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
