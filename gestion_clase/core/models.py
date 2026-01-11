f4bd35112756d1bd3b548baac4a8b177fa2d1f32
dperezld <dperezld@fpvirtualaragon.es>
2026-01-11 01:00:05 +0100

    Postgre

--- gestion_clase/core/models.py
+++ gestion_clase/core/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    ES_PROFESOR = 'PR'
+    ES_ALUMNO = 'AL'
+    ROLES = [
+        (ES_PROFESOR, 'Profesor'),
+        (ES_ALUMNO, 'Alumno'),
+    ]
+    rol = models.CharField(max_length=2, choices=ROLES, default=ES_ALUMNO)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_rol_display()})"
--- gestion_clase/gestion_clase/settings.py
+++ gestion_clase/gestion_clase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'gestion_clase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'practicaut04_db', 
+        'USER': 'postgres',        # Usuario por defecto
+        'PASSWORD': 'admin', # Tu contraseña de Postgres
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'core.Usuario'
