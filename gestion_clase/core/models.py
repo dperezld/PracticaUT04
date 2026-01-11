54c5d5111f3651af8e1d13c996be05a9a939be85
dperezld <dperezld@fpvirtualaragon.es>
2026-01-11 01:05:40 +0100

    Tareas

--- gestion_clase/core/models.py
+++ gestion_clase/core/models.py
@@ -12,3 +12,21 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.get_rol_display()})"
+    
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_limite = models.DateTimeField()
+    creado_por = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='tareas_creadas')
+    finalizada = models.BooleanField(default=False)
+    requiere_evaluacion = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+
+class TareaIndividual(Tarea):
+    alumno_asignado = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='tareas_individuales')
+
+class TareaGrupal(Tarea):
+    alumnos = models.ManyToManyField('Usuario', related_name='tareas_grupales')
